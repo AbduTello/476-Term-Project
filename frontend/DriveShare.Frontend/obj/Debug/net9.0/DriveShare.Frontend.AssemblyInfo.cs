8c1e5b003189031b2792b2a7ef989eb78b6b6907
khanabdu25 <khanabdu@umich.edu>
2025-04-05 22:28:16 -0400

    log jwt token of logged in user

--- frontend/DriveShare.Frontend/obj/Debug/net9.0/DriveShare.Frontend.AssemblyInfo.cs
+++ frontend/DriveShare.Frontend/obj/Debug/net9.0/DriveShare.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriveShare.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faacfebee9c74fa27dd66a84fdfbd5b8fd263d9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a2dbd5dc3a359030eeefa2a2a35d6a0572f61e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriveShare.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriveShare.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
