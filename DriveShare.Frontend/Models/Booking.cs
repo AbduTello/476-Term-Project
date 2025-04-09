0a68e6d64863482b584de9cd5935b9bcae5f1128
khanabdu25 <khanabdu@umich.edu>
2025-04-09 17:28:17 -0400

    wire up bookcar method to backend

---
+++ DriveShare.Frontend/Models/Booking.cs
@@ -0,0 +1,22 @@
+namespace DriveShare.Frontend.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public CarListing Car { get; set; }
+        public BNUser Renter { get; set; }
+        public BNUser Owner { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public BookingStatus Status { get; set; }
+
+        public enum BookingStatus
+        {
+            Pending,
+            Confirmed,
+            Completed,
+            Canceled
+        }
+    }
+}
